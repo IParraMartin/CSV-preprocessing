65eb52f66db7e79a6d51737b607d6913ab424ce8
IParraMartin <82852463+IParraMartin@users.noreply.github.com>
2023-04-05 11:02:50 -0500

    generalized to insert columns to be deleted in other cases

--- cleaner.py
+++ cleaner.py
@@ -3,17 +3,20 @@ import re
 from cleantext import clean
 
 
-def file_cleaner(files):
+def file_cleaner(files, del_column=None):
+
     n = 0
 
     for file in files:
 
         df = pd.read_csv(file)
 
-        list_cols = ['Unnamed: 0', 'created_at', 'edit_history_tweet_ids', 'id', 'withheld']
+        list_cols = del_column
 
         for col in list_cols:
+
             if col in df.columns:
+
                 del df[col]
 
         df['text'] = df['text'].str.lower()
@@ -23,8 +26,9 @@ def file_cleaner(files):
         df['text'] = df['text'].apply(lambda x: re.sub('[0-9]', '', x))
         df['text'] = df['text'].apply(lambda x: clean(x, no_emoji=True))
 
-        n += 1  
+        n += 1
         df.to_csv('set-tweets-' + str(n) + '.csv', index=False)
+
 
 all_files = []
 file_cleaner(all_files)
